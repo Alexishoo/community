1ce417bd9a423d065cf5f3e01493d6133a47d74e
Insomnia_ <wu_zh@yahoo.com>
2017-06-16 19:52:53 +0800

    seed files 0.0.1

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,7 +1,8 @@
 import {AppComponent} from './app.component';
 import {AppHeader} from './header/header.component'
-
+import {AppFouter} from './footer/footer.component';
 export const components = [
   AppComponent,
-  AppHeader
+  AppHeader,
+  AppFouter
 ];
